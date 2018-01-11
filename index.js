8b57d309aab70514382bf5763af580d2066fee12
Michael Myung <michael.h.myung@gmail.com>
2018-01-11 05:23:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObj = {}
-  Object.assign(newObj, object, {[key]:value})
-  delete newObj.key
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
   return newObj
 }
 
